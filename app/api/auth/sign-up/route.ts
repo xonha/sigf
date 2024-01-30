822a22252deba395981381f23e813f474b28cac8
henrique <henrique@xonha.space>
2024-01-30 18:59:51 -0300

    feat: login page

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -5,35 +5,37 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
-  const requestUrl = new URL(request.url);
-  const formData = await request.formData();
-  const email = String(formData.get("email"));
-  const password = String(formData.get("password"));
-  const supabase = createRouteHandlerClient({ cookies });
+    const requestUrl = new URL(request.url);
+    const formData = await request.formData();
+    const email = String(formData.get("email"));
+    const password = String(formData.get("password"));
+    const supabase = createRouteHandlerClient({ cookies });
 
-  const { error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      emailRedirectTo: `${requestUrl.origin}/auth/callback`,
-    },
-  });
+    console.log("supabase", supabase);
 
-  if (error) {
-    return NextResponse.redirect(
-      `${requestUrl.origin}/?error=Could not authenticate user`,
-      {
-        // a 301 status is required to redirect from a POST to a GET route
-        status: 301,
-      }
-    );
-  }
+    const { error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            emailRedirectTo: `${requestUrl.origin}/auth/callback`,
+        },
+    });
 
-  return NextResponse.redirect(
-    `${requestUrl.origin}/?message=Check email to continue sign in process`,
-    {
-      // a 301 status is required to redirect from a POST to a GET route
-      status: 301,
+    if (error) {
+        return NextResponse.redirect(
+            `${requestUrl.origin}/login?error=Could not authenticate user`,
+            {
+                // a 301 status is required to redirect from a POST to a GET route
+                status: 301,
+            }
+        );
     }
-  );
+
+    return NextResponse.redirect(
+        `${requestUrl.origin}/?message=Check email to continue sign in process`,
+        {
+            // a 301 status is required to redirect from a POST to a GET route
+            status: 301,
+        }
+    );
 }
