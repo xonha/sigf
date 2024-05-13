77f15f29936c5247c3a3f891ea0bd86d2faf6a2e
Henrique <henrique@xonha.space>
2024-05-13 08:48:42 -0300

    fix: calendar sidebar showing for admin only

--- app/(pages)/calendar/components/Sidebar.tsx
+++ app/(pages)/calendar/components/Sidebar.tsx
@@ -1,4 +1,11 @@
+import { usersAtom } from "@/app/atoms/usersAtom";
+import { useRecoilValue } from "recoil";
+
 export default function Sidebar(props: { children: React.ReactNode }) {
+  const user = useRecoilValue(usersAtom);
+
+  if (user?.userRole !== "admin") return null;
+
   return (
     <aside className="sticky w-52 hidden xl:block h-[calc(100vh-64px)] border-gray-300 border-l overflow-hidden">
       {props.children}
