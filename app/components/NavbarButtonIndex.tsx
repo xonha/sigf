43f0b39da3f02cccb03e0e7296465c760bb667aa
Henrique <henrique@xonha.space>
2024-08-06 12:46:06 -0300

    refactor: Minhas Presenças button

--- app/components/NavbarButtonIndex.tsx
+++ app/components/NavbarButtonIndex.tsx
@@ -48,43 +48,46 @@ export default function NavbarButtonIndex() {
         </button>
       </>
     );
-  } else if (pathName.match(classesIdRegex) && userRole === "admin") {
-    return (
-      <div className="flex gap-4">
-        <div
-          className={
-            enrollmentCount.leader >= enrollmentCount.half
-              ? "bg-blue-300  text-white font-bold py-2 px-4 rounded"
-              : "bg-blue-500  text-white font-bold py-2 px-4 rounded"
-          }
-        >
-          Condutores(as): {enrollmentCount.leader} / {enrollmentCount.half}
-        </div>
-        <Link
-          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-          href={`${pathName}/attendance`}
-        >
-          Presenças
-        </Link>
-        <div
-          className={
-            enrollmentCount.led >= enrollmentCount.half
-              ? "bg-orange-300  text-white font-bold py-2 px-4 rounded"
-              : "bg-orange-500  text-white font-bold py-2 px-4 rounded"
-          }
-        >
-          Conduzidos(as): {enrollmentCount.led} / {enrollmentCount.half}
-        </div>
-      </div>
-    );
   } else if (pathName.match(classesIdRegex)) {
     return (
-      <Link
-        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-        href={`${pathName}/userAttendance`}
-      >
-        Minhas presenças
-      </Link>
+      <div className="flex gap-4">
+        {userRole === "admin" && (
+          <div
+            className={
+              enrollmentCount.leader >= enrollmentCount.half
+                ? "bg-blue-300  text-white font-bold py-2 px-4 rounded"
+                : "bg-blue-500  text-white font-bold py-2 px-4 rounded"
+            }
+          >
+            Condutores(as): {enrollmentCount.leader} / {enrollmentCount.half}
+          </div>
+        )}
+        <Link
+          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+          href={`${pathName}/userAttendance`}
+        >
+          Minhas presenças
+        </Link>
+        {userRole === "admin" && (
+          <>
+            <Link
+              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+              href={`${pathName}/attendance`}
+            >
+              Presenças da turma
+            </Link>
+            <div
+              className={
+                enrollmentCount.led >= enrollmentCount.half
+                  ? "bg-orange-300  text-white font-bold py-2 px-4 rounded"
+                  : "bg-orange-500  text-white font-bold py-2 px-4 rounded"
+              }
+            >
+              Conduzidos(as): {enrollmentCount.led} / {enrollmentCount.half}
+            </div>
+          </>
+        )}
+      </div>
     );
   } else if (pathName.match(attendanceRegex) && userRole === "admin") {
     return <GenerateClassDates />;
